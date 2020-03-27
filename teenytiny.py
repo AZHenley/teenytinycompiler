c99e83ff1fc84e374ed8b6e256fc7a87e2dc066e
Austin Henley <azh321@gmail.com>
2020-03-27 00:39:19 -0400

    Initial outline of program structure.

---
+++ teenytiny.py
@@ -0,0 +1,33 @@
+import enum
+
+def main():
+    # Read.
+    # Lex.
+    # Parse.
+    # Generate.
+    # Write.
+    pass
+
+def abort():
+    pass
+
+
+# Lexer.
+
+class Lexer:
+    def currentTokenIs(self, token):
+        pass
+
+    def consume(self, token):
+        pass
+
+
+# Token.
+class Token(enum.Enum):
+    pass
+
+class Emitter():
+    pass
+
+
+
